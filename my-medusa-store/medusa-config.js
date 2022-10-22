33274835d77a4758564b64ab2fd7e23bd5952c4f
Jhanvi <jhanvi.ug20@nsut.ac.in>
2022-10-22 22:00:49 +0530

    Deploy Medusa App on Heroku

--- my-medusa-store/medusa-config.js
+++ my-medusa-store/medusa-config.js
@@ -67,3 +67,18 @@ module.exports = {
   },
   plugins,
 };
+
+module.exports = {
+  projectConfig: {
+    redis_url: REDIS_URL,
+    database_url: DATABASE_URL,
+    database_type: "postgres",
+    store_cors: STORE_CORS,
+    admin_cors: ADMIN_CORS,
+    database_extra:
+      process.env.NODE_ENV !== "development"
+        ? { ssl: { rejectUnauthorized: false } }
+        : {},
+  },
+  plugins,
+}
